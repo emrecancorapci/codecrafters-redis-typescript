0d99894db0d71effa6dcace0c8d2c6b1c223b120
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-04-08 15:40:12 +0300

    Update serverListener to be an async function

--- app/main.ts
+++ app/main.ts
@@ -3,10 +3,10 @@ import * as net from "node:net";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 // console.log("Logs from your program will appear here!");
 
-function serverListener(connection: net.Socket) {
+async function serverListener(connection: net.Socket) {
     // connection is a Socket object that can be used to read and write data from the client
     // connection.on is used to register a callback that will be called when data is received from the client
-    connection.on("data", function onConnection(data: Buffer) {
+    connection.on("data", async function onConnection(data: Buffer) {
       // connection.write is used to write data to the client
       connection.write("+PONG\r\n");
     });
