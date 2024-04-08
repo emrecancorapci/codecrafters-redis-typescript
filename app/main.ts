a9fb2b981d08943db434db185282cbc6ed80591f
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-04-08 15:18:36 +0300

    Refactor server listener function in main.ts, update name in package.json

--- app/main.ts
+++ app/main.ts
@@ -3,10 +3,13 @@ import * as net from "node:net";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 // console.log("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
-const server: net.Server = net.createServer((connection: net.Socket) => {
-  // Handle connection
-});
+function serverListener(connection: net.Socket) {
+    connection.on("data", function onConnection(data: Buffer) {
+      data ? connection.write(data) : connection.write("+PONG\r\n");
+    });
+}
 
-server.listen(6379, "127.0.0.1");
+const server: net.Server = net.createServer(serverListener);
+
+server.listen(6379, "127.0.0.1");   
 
