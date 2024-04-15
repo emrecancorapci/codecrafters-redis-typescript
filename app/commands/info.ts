54dec09855fb1250a28fe613f59c0dff5acfe584
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-04-16 02:57:14 +0300

    Refactor info command to handle replication argument and use getReplicaOf function

--- app/commands/info.ts
+++ app/commands/info.ts
@@ -1,10 +1,17 @@
 import RESPV2Serializer from '../resp-v2-serializer.ts';
+import getReplicaOf from '../server/arguments/get-replicaof.ts';
 import { DataType, ServerAction } from '../types.ts';
 
 const info: ServerAction = (data: DataType[]) => {
-  return data[0] === 'replication'
-    ? { value: RESPV2Serializer.serializeBulk('role:master') }
-    : { error: 'Unknown INFO argument. Please provide a valid argument.' };
+  const masterInfo = getReplicaOf();
+
+  if (data[0] === 'replication') {
+    return masterInfo
+      ? { value: RESPV2Serializer.serializeBulk('role:slave') }
+      : { value: RESPV2Serializer.serializeBulk('role:master') };
+  }
+
+  return { error: 'Unknown INFO argument. Please provide a valid argument.' };
 };
 
 export default info;
