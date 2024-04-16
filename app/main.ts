25fb05e7c3bc178468c837c2633051ed4893617f
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-04-16 18:54:45 +0300

    Refactor main.ts for master handshake

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,20 @@
 import * as net from 'node:net';
 
+import RESPV2Serializer from './resp-v2-serializer.ts';
 import getPort from './server/arguments/get-port.ts';
+import getReplicaOf from './server/arguments/get-replicaof.ts';
 import serverListener from './server/listener.ts';
 
 const PORT = getPort();
+const master = getReplicaOf();
+
+if (master) {
+  const client = new net.Socket();
+
+  client.connect(master.port, master.host, () => {
+    client.write(RESPV2Serializer.serializeString('PING'));
+  });
+}
 
 const server: net.Server = net
   .createServer(serverListener)
