a105fe81e235bf104cb1dae0f24c7341518fbc2a
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-04-16 21:29:16 +0300

    Add master handshake

--- app/main.ts
+++ app/main.ts
@@ -1,26 +1,16 @@
 import * as net from 'node:net';
 
 import getPort from './server/arguments/get-port.ts';
-import getReplicaOf from './server/arguments/get-replicaof.ts';
 import ServerListener from './server/server-listener.ts';
 
 const PORT = getPort();
-const master = getReplicaOf();
-
-if (master) {
-  const client = new net.Socket();
-
-  client.connect(master.port, master.host, () => {
-    client.write(RESPV2Serializer.serializeArray(['PING']));
-  });
-}
 
 const server: net.Server = net
   .createServer()
   .on('connection', (socket) => {
+    console.log(`New connection from`, socket.remoteAddress, socket.remotePort);
     const serverListener = new ServerListener(socket);
     serverListener.listen();
-    console.log(`New connection from`, socket.remoteAddress, socket.remotePort);
   })
   .on('error', (error) => console.error(error));
 
--- app/server/server-listener.ts
+++ app/server/server-listener.ts
@@ -3,10 +3,12 @@ import * as net from 'node:net';
 import RESPV2Parser from '../resp-v2-parser.ts';
 import RESPV2Serializer from '../resp-v2-serializer.ts';
 import { DatabaseValue, DataType } from '../types.ts';
+import getReplicaOf from './arguments/get-replicaof.ts';
 import ServerHandler from './handler.ts';
 
 export default class ServerListener {
   private readonly database: Map<string, DatabaseValue>;
+  private readonly master: { host: string; port: number } | undefined;
   private readonly socket: net.Socket;
   private readonly serverHandler: ServerHandler;
 
@@ -18,10 +20,22 @@ export default class ServerListener {
       database: this.database,
       socketWrite: (data: string) => socket.write(data),
     });
+
+    this.master = getReplicaOf();
   }
 
   public listen(): void {
+    this.performHandshake();
     this.socket.on('data', this.onSocketData.bind(this)).on('error', (error) => console.error(error));
+  }
+
+  private performHandshake(): void {
+    if (!this.master) return;
+
+    this.socket.connect(this.master.port, this.master.host, () => {
+      this.socket.write(RESPV2Serializer.serializeArray(['PING']));
+      this.socket.on('data', this.onHandshakeData.bind(this));
+    });
   }
 
   private onSocketData(buffer: Buffer): void {
@@ -34,6 +48,21 @@ export default class ServerListener {
     } catch (error) {
       console.error(error);
       if (error instanceof Error) return this.sendError('ERROR: ' + error.message);
+    }
+  }
+
+  private onHandshakeData(buffer: Buffer): void {
+    if (!this.master) return;
+
+    const parsedBuffer = this.parseBuffer(buffer);
+    if (parsedBuffer === undefined) return;
+    const { operation } = parsedBuffer;
+
+    if (operation === 'PONG') {
+      this.socket.write(RESPV2Serializer.serializeArray(['REPLICAOF', 'listening-port', this.master.port.toString()]));
+      this.socket.write(RESPV2Serializer.serializeArray(['REPLICAOF', 'capa', 'psync2']));
+    } else {
+      return this.sendError('Invalid response from master');
     }
   }
 
