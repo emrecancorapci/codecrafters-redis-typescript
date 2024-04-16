a7de9dd6eec61754b2ae474a667cb2f8c2c59081
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-04-16 21:28:59 +0300

    Refactor ServerListener class to separate parsing operation to a new function

--- app/server/server-listener.ts
+++ app/server/server-listener.ts
@@ -2,37 +2,34 @@ import * as net from 'node:net';
 
 import RESPV2Parser from '../resp-v2-parser.ts';
 import RESPV2Serializer from '../resp-v2-serializer.ts';
-import { DatabaseValue } from '../types.ts';
+import { DatabaseValue, DataType } from '../types.ts';
 import ServerHandler from './handler.ts';
 
 export default class ServerListener {
-  database: Map<string, DatabaseValue>;
-  socket: net.Socket;
-  serverHandler: ServerHandler;
+  private readonly database: Map<string, DatabaseValue>;
+  private readonly socket: net.Socket;
+  private readonly serverHandler: ServerHandler;
 
   constructor(socket: net.Socket) {
-    this.socket = socket;
     this.database = new Map<string, DatabaseValue>();
+    this.socket = socket;
+
     this.serverHandler = new ServerHandler({
       database: this.database,
       socketWrite: (data: string) => socket.write(data),
     });
   }
 
-  public listen() {
+  public listen(): void {
     this.socket.on('data', this.onSocketData.bind(this)).on('error', (error) => console.error(error));
   }
 
-  private onSocketData(buffer: Buffer) {
+  private onSocketData(buffer: Buffer): void {
     try {
-      const parsedBuffer = RESPV2Parser.parse(buffer.toString());
+      const parsedBuffer = this.parseBuffer(buffer);
+      if (parsedBuffer === undefined) return;
 
-      if (parsedBuffer === undefined)
-        return this.sendError('Parser returns nothing. Please provide a valid RESPv2 data.');
-      if (!Array.isArray(parsedBuffer)) return this.sendError('Invalid data. Please provide a valid RESPv2 array.');
-      if (typeof parsedBuffer[0] !== 'string') return this.sendError('Command should be a string.');
-
-      const [operation, ...data] = parsedBuffer;
+      const { operation, data } = parsedBuffer;
       this.serverHandler.run(operation, data);
     } catch (error) {
       console.error(error);
@@ -40,7 +37,28 @@ export default class ServerListener {
     }
   }
 
-  private sendError(message: string) {
+  private parseBuffer(buffer: Buffer): ParseServerBuffer {
+    const parsedBuffer = RESPV2Parser.parse(buffer.toString());
+
+    if (parsedBuffer === undefined) {
+      this.sendError('Parser returns nothing. Please provide a valid RESPv2 data.');
+      return;
+    }
+    if (!Array.isArray(parsedBuffer)) {
+      this.sendError('Invalid data. Please provide a valid RESPv2 array.');
+      return;
+    }
+    if (typeof parsedBuffer[0] !== 'string') {
+      this.sendError('Command should be a string.');
+      return;
+    }
+
+    return { operation: parsedBuffer[0], data: parsedBuffer.slice(1) };
+  }
+
+  private sendError(message: string): void {
     this.socket.write(RESPV2Serializer.serializeError(message));
   }
 }
+
+type ParseServerBuffer = { operation: string; data: DataType[] } | undefined;
